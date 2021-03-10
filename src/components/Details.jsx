77a43924a354b520aa28b5a6071ffa5602e2d39d
ikatz87 <ilija.pavlovic87@gmail.com>
2021-03-10 23:25:21 +0100

    Add components / Delete unused files / Create Entry componenet

---
+++ src/components/Details.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Details() {
+  return (
+    <dt>
+      <span className="emoji" role="img" aria-label="Tense Biceps">
+        💪
+      </span>
+      <span>Tense Biceps</span>
+    </dt>
+  );
+}
+
+export default Details;
---
+++ src/components/Entry.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Entry() {
+  return <h1></h1>;
+}
+
+export default Entry;
